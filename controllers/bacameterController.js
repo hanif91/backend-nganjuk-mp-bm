bc9941d93ff341eeaa4978f997eebd207551c36a
Ferdinand Felix <ffelixhermawan@gmail.com>
2025-08-29 08:55:20 +0700

    LOGGING UPLOADHASILBACA

--- controllers/bacameterController.js
+++ controllers/bacameterController.js
@@ -98,6 +98,8 @@ async function uploadHasilBaca(req, res) {
     longitude,
   } = req.body || {};
 
+  console.log(req.body);
+
   if (!tgl) return res.status(400).json({ message: "tgl wajib diisi" });
   if (!moment(tgl).isValid())
     return res.status(400).json({ message: "tgl tidak valid" });
